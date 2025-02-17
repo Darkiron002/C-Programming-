971b19bbf04ad10d99c8473028e48003ff65318e
Darkiron002 <chayanmistry14@gmail.com>
2025-02-17 22:21:54 +0530

    First commit

--- test.c
+++ test.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
-int main() {
-    printf("Hello, World! this is fucking iritating  but still I'm doing it  \n");
-    return 0;
-}
+#include<stdio.h>
+int main(){
+	int age = 22;
+	float pi = 3.14;
+	char star = '*';
+	printf("I am %d years old \n ",age);
+	printf("the value of pi is %f \n",pi);
+	printf("The star is shown as %c \n",star);
+	return 0;
+	}
