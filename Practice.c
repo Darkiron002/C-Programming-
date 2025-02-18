8958f2a96e135cf7f34f0d977891f86ae3370aca
Darkiron002 <chayanmistry14@gmail.com>
2025-02-18 23:27:21 +0530

    Area of a Square

---
+++ Practice.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    float side;
+    printf("Enter your disired side: ");
+    scanf("%f", &side);
+
+    printf("The area of the squre is : %f", side * side);
+    return 0; 
+}
