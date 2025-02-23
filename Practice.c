f450784df0bf31cdefa328a8e32f7c6d23b43d4a
Darkiron002 <chayanmistry14@gmail.com>
2025-02-23 23:33:26 +0530

    Avarage of 3 numners

--- Practice.c
+++ Practice.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 int main() {
     int a;
+    int b;
+    int c;
     printf("Enter your number:");
     scanf("%d", &a);
-    printf("%d", (a > 9) && (a<100));
+    printf("Enter your number:");
+    scanf("%d", &b);
+    printf("Enter your number:");
+    scanf("%d", &c);
+    printf("The avarage of the numbers is : %d", (a+b+c)/3);
     return 0; 
 }
