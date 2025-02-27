944b3fd2d41a5bf4d60ace90ba3c3de5fade64f3
Darkiron002 <chayanmistry14@gmail.com>
2025-02-27 10:26:25 +0530

    while loop

--- Practice.c
+++ Practice.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main () {
-	for(int i=0; i <=10; i = i+1){
-		printf("%d \n", i );
-		}
+	int i= 1; //Declaration 
+    while(i<=5) { // Termination
+ 		printf("hello world \n");
+		i++; //updation
+    }
 	return 0;
 }	 
