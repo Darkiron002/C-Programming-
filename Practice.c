e5f7344d0abb495b0136c543056fd594b703cb28
Darkiron002 <chayanmistry14@gmail.com>
2025-02-27 10:11:33 +0530

    for loop

--- Practice.c
+++ Practice.c
@@ -1,25 +1,7 @@
 #include<stdio.h>
-int main() {
-    int day; 
-printf("enter day(1-7): ");
-scanf("%d", &day);
-switch(day){
-	case 1: printf("monday \n");
-			break;
-	case 2: printf("tuesday \n");
-			break;
-	case 3: printf("wednesday \n");
-			break;
-	case 4: printf("thusday \n");
-			break;
-	case 5: printf("friday \n");
-			break;
-	case 6: printf("saturday \n");
-			break;
-	case 7: printf("sunday \n");
-			break;
-	default: printf("not valid \n");
-			break;
-	}
-    return 0;
-}
+int main () {
+	for(int i=0; i <=10; i = i+1){
+		printf("%d \n", i );
+		}
+	return 0;
+}	 
