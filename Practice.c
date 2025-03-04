5f6aac8aea4698016a704e22957827cb6a7bf504
Darkiron002 <chayanmistry14@gmail.com>
2025-03-05 00:30:00 +0530

    Arguments v Parameter

--- Practice.c
+++ Practice.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int sum(int a, int b);
+void printTable(int n);  // Function prototype
 int main() {
-	int a, b;
-	printf("Enter number a: ");
-	scanf("%d", &a);
-	printf("Enter number b: ");	
-	scanf("%d", &b);
-	int s = sum(a, b);
-	printf("Sum of %d and %d is %d\n", a, b, s);
+	int n;
+	printf("Enter the number to print the table: ");
+	scanf("%d", &n);
+	printTable(n);       // Argument/ actual parameter
 	return 0;
 }
-int sum(int x, int y) {
-	return x + y;
+void printTable(int n) {  // Parameter/ formal parameter
+	int i;
+	for(i=1; i<=10; i++) {
+		printf("%d \n ", n*i);
+	}
 }
