1afe3d547945e2d3f9333257be0f94f4ad92bd67
Darkiron002 <chayanmistry14@gmail.com>
2025-03-05 00:23:01 +0530

    Add function with two veriables

--- Practice.c
+++ Practice.c
@@ -1,10 +1,16 @@
-#include<stdio.h>
-int main () {
-	int n;
-	printf("Enter your desired number: ");
-	scanf("%d", &n);
-	for(int i= 1; i<= 10; i++){
-		printf("%d\n", n*i);
-	}
+#include <stdio.h>
+
+int sum(int a, int b);
+int main() {
+	int a, b;
+	printf("Enter number a: ");
+	scanf("%d", &a);
+	printf("Enter number b: ");	
+	scanf("%d", &b);
+	int s = sum(a, b);
+	printf("Sum of %d and %d is %d\n", a, b, s);
 	return 0;
-}	 
+}
+int sum(int x, int y) {
+	return x + y;
+}
