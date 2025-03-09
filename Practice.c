d8c52c160365ee499c9acd40f26a28ecc723ea37
Darkiron002 <chayanmistry14@gmail.com>
2025-03-10 00:30:03 +0530

    Sum of N natural numbers

--- Practice.c
+++ Practice.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
+int sum(int n);
 
-void printTable(int n);  // Function prototype
 int main() {
-	int n;
-	printf("Enter the number to print the table: ");
-	scanf("%d", &n);
-	printTable(n);       // Argument/ actual parameter
-	return 0;
+	printf("sum is : %d", sum(5));
 }
-void printTable(int n) {  // Parameter/ formal parameter
-	int i;
-	for(i=1; i<=10; i++) {
-		printf("%d \n ", n*i);
+
+int sum(int n) {
+	if (n == 1) {
+		return 1;
 	}
+	int sumNm1 = sum(n - 1);
+	return n + sumNm1;
 }
