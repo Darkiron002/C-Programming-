c961505c9e00203e33ce05ae251f3bbb190ed539
Darkiron002 <chayanmistry14@gmail.com>
2025-03-17 00:10:45 +0530

    Pointer Arithmatic

--- Practice.c
+++ Practice.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
-
-void All_in_finction(int a, int b, int *sum , int *product, int *avag);
-
 int main() {
-	int a= 5, b= 10;
-	int sum, product, avag;
-	All_in_finction(a, b, &sum, &product, &avag);
-	printf("Sum: %d\nProduct: %d\nAvag: %d\n", sum, product, avag);
-	return 0;
-}
-
-void All_in_finction(int a, int b ,int *sum , int *product, int *avag) {
-	*sum = a + b;
-	*product = a * b;
-	*avag = (a + b) / 2;
+  int age = 22;
+  int *ptr = &age;
+  printf("ptr - %u\n", ptr);
+  ptr++;
+  printf("ptr - %u\n", ptr);
+  return 0;
 }
