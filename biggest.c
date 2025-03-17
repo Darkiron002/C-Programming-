0e0e0bfabed8c9f0ad5f6f6f6f65bd4a31c0448e
Darkiron002 <chayanmistry14@gmail.com>
2025-03-17 13:33:14 +0530

    Biggest between the three numbers

--- biggest.c
+++ biggest.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 int main() {
-  int a,b,c;
+  int a, b, c;
   printf("Enter the values of a: ");
   scanf("%d", &a);
   printf("Enter the values of b: ");
   scanf("%d", &b);
   printf("Enter the values of c: ");
   scanf("%d", &c);
-  if (a>b && a>c ) {
+  if (a > b && a > c) {
     printf("a is the largest number \n");
-  } else if (b >a && b >c ) {
+  } else if (b > a && b > c) {
     printf("b is the largest number \n");
   } else {
     printf("c is the largest number \n");
