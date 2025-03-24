647fd40a0945d0f87c47a434f7c7f3a3b1310bf4
Darkiron002 <chayanmistry14@gmail.com>
2025-03-24 22:39:19 +0530

    Question of Printing

--- Practice.c
+++ Practice.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-int main () {
-  int a;
-  printf("Enter a number: ");
-  scanf("%d", &a);
-  int reversed, remainder;
-  
-  reversed = 0;
-  remainder = 0;
-  while (a != 0) {
-    remainder = a % 10;
-    reversed = reversed * 10 + remainder;
-    a /= 10;
-  }
-  printf("Reversed number is: %d \n", reversed);
-  return 0;
-  
-}
+
+int main() {
+    // Write code here
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    for (int i = 1; i <= num; i++) {
+        if (i % 5 == 0 ||i % 7 == 0) continue;
+        printf("%d \n",i);
+    }
+    return 0;
+};
