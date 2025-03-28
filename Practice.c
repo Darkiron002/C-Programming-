0088d8877c259367df9f791cef30dbaf093dc4f4
Darkiron002 <chayanmistry14@gmail.com>
2025-03-28 08:52:43 +0530

    Program to count the number of even numbers in an array

--- Practice.c
+++ Practice.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+int counteven(int arr[],int n);
 int main() {
-    // Write code here
-    int num;
-    printf("Enter a number: ");
-    scanf("%d",&num);
-    for (int i = 1; i <= num; i++) {
-        if (i % 5 == 0 ||i % 7 == 0) continue;
-        printf("%d \n",i);
-    }
+    //Program to count the number of even numbers in an array
+    
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    counteven(arr,10);
+    printf("%d",counteven(arr,10));
+    
+    
     return 0;
-};
+}
+int counteven(int arr[],int n) {
+    int count = 0;
+    for(int i = 0;i < n;i++) {
+        if(arr[i] % 2 == 0) {
+            count++;
+        }
+    }
+
+    return count; 
+}
