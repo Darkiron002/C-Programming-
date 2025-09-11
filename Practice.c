4ed14e23ead688beeda2a39cd982c6e7d5536bc3
Darkiron002 <chayanmistry14@gmail.com>
2025-09-11 10:00:40 +0530

    even odd counter

--- Practice.c
+++ Practice.c
@@ -1,31 +1,30 @@
-# include<stdio.h>
-int main (){
-    int mat[3][3], i, j, sum = 0;
-    int matb[3][3];
-    int matc[3][3];
-    printf("Enter the elements of the 3x3 matrix:\n");
-    for(i = 0; i < 3; i++){
-        for(j = 0; j < 3;j++){
-            printf("The elements of the matrix are: %d, %d ",i, j);
-            scanf("%d", &mat[i][j]);
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+
+    float average, averageo;
+    int even = 0;
+    int odd = 0;
+    int avg_e=0;
+    int avg_o=0;
+    int n = 0;
+    while(n!= 0){
+        scanf("%d",&n);
+        if(n %2 ==0){
+            even++;
+            avg_e = avg_e + n;
         }
+        else if (n %2 !=0){
+            odd++;
+            avg_o = avg_o +n;        }
     }
-    for(i = 0; i < 3; i++){
-        for(j = 0; j < 3;j++){
-            printf("The elements of the matrix are: %d, %d ",i, j);
-            scanf("%d", &matb[i][j]);
-        }
-    }
-    for(i = 0; i < 3; i++){
-        for(j = 0; j < 3;j++){
-            matc[i][j] = mat[i][j] + matb[i][j];
-        }
-    }
-    printf("Resultant matrix after addition:\n");
-    for(i = 0; i < 3; i++){
-        for(j = 0; j < 3;j++){
-            printf("%d ", matc[i][j]);
-        }printf(" \n");
-    }
+    average = avg_e/even;
+    averageo = avg_o/ odd;
+    printf("%d \n",even);
+    printf("%d \n",odd);
+    printf("%f \n",average);
+    printf("%f \n",averageo);
+
     return 0;
 }
